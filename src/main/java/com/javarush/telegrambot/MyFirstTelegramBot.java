9ad186c2af517f731fa863a95444a625bd6aed35
Boris Kuchin <bvkuchin@gmail.com>
2024-04-23 16:48:55 +0300

    buttins added

--- src/main/java/com/javarush/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/com/javarush/telegrambot/MyFirstTelegramBot.java
@@ -8,6 +8,8 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.util.Map;
+
 public class MyFirstTelegramBot extends MultiSessionTelegramBot {
     public static final String NAME = "BorisBDay_bot"; // TODO: добавьте имя бота в кавычках
     public static final String TOKEN = "5229040491:AAE24EGFxrDgcHahZNgaaro52pfTNTsvIL0"; //TODO: добавьте токен бота в кавычках
@@ -18,11 +20,31 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
     @Override
     public void onUpdateEventReceived(Update updateEvent) {
-        // TODO: основной функционал бота будем писать здесь
-        sendTextMessageAsync("Привет, ползователь");
-        sendTextMessageAsync("И еще раз");
+        if (getMessageText().equals("/start")) {
+            sendTextMessageAsync("HiHi");
+        }
+        if (getMessageText().equals("/buy")) {
+            sendTextMessageAsync("Asta la vista, baby!");
+        }
 
-    }
+        if (getMessageText().contains("cat")) {
+            sendTextMessageAsync("Ваше любимое животное? ",
+                    Map.of("Кот", "callbackCat",
+                            "Собака", "callbackDog"));
+
+        }
+        if (getCallbackQueryButtonKey().equals("callbackCat")) {
+            sendPhotoMessageAsync("step_4_pic");
+        } else if (getCallbackQueryButtonKey().equals("callbackDog")) {
+            sendPhotoMessageAsync("step_6_pic");
+        }
+
+
+        if (getMessageText().equals("Smile")) {
+            var msg = getLastSentMessage();
+            editTextMessageAsync(msg.getMessageId(), msg.getText() + " :-) ");
+        }
+        }
 
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
